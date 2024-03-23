2bd87db444847ee01e19e8a81bd0eefff386411d
Aayush Jain <aayush.jain@rapipay.com>
2024-03-23 22:40:39 +0530

    Removed unused code

--- app/src/main/java/com/test/pushnotifications/MainActivity.kt
+++ app/src/main/java/com/test/pushnotifications/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
 import com.test.pushnotifications.ui.theme.PushNotificationsTheme
@@ -68,12 +67,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PushNotificationsTheme {
-        Greeting("Android")
-    }
 }
